963aa30c3f09ac928606dda33f9b7d04c34a33bb
Juan Sebastian Medina <jsemedina.21@gmail.com>
2024-01-25 11:44:46 -0500

    Will add function to fetch and validate lab code number

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -12,27 +12,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _accountNumController = TextEditingController();
+  final _labCodeNumController = TextEditingController();
+  String? _labCodeNumber;
   String? _error;
   String? _accountNumber;
-  bool isAddingLab = false;
+  bool _isAddingLab = false;
   Map<String, dynamic>? _accountInfo;
+  List<dynamic>? _labCode;
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _accountNumController.addListener(getAccountNum);
+  // }
 
-  @override
-  void initState() {
-    super.initState();
-    _accountNumController.addListener(getAccountNum);
-  }
-
-  void getAccountNum() {
-    _accountNumber = _accountNumController.text;
-  }
-
-  void _addNewLab(){
-    setState(() {
-      isAddingLab = true;
-    });
-    print(isAddingLab);
-  }
+  // void getAccountNum() {
+  //   _accountNumber = _accountNumController.text;
+  // }
 
   Future<void> getAccountData(String accountNum) async {
     if (_accountNumber!.trim().isEmpty) {
@@ -71,10 +66,40 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  @override
-  void dispose() {
-    _accountNumController.dispose();
-    super.dispose();
+  Future<void> validateLab(String labCode) async {
+    if (_labCodeNumber!.trim().isEmpty) {
+      setState(() {
+        _error = "Please Enter a Lab Code.";
+        _labCodeNumController.clear();
+      });
+      return;
+    }
+    final url = Uri.https("cycling-api.cvoapis.com", "/magento/lab/$labCode");
+
+    try {
+      final response = await http.get(url);
+      print(response.body);
+      if (response.statusCode == 200 && response.body != '[]') {
+        setState(() {
+          _labCode = json.decode(response.body);
+          _labCodeNumController.clear();
+        });
+      } else if (response.statusCode >= 400) {
+        setState(() {
+          _error = "Failed to fetch data";
+        });
+        print(_error);
+      } else if (response.body == '[]') {
+        setState(() {
+          _labCode = null;
+          _labCodeNumController.clear();
+          _error = "Lab Code number doesn't exist.";
+        });
+        print(_error);
+      }
+    } catch (e) {
+      print("Something went wrong. $e");
+    }
   }
 
   @override
@@ -106,6 +131,7 @@ class _HomePageState extends State<HomePage> {
                   width: 250,
                   child: TextField(
                     onSubmitted: (value) {
+                      _accountNumber = _accountNumController.text;
                       getAccountData(_accountNumber!);
                     },
                     controller: _accountNumController,
@@ -183,13 +209,19 @@ class _HomePageState extends State<HomePage> {
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           TextButton.icon(
-                            onPressed: _addNewLab,
-                
-                            icon: const Icon(Icons.add_circle,
+                            onPressed: () {
+                              setState(() {
+                                _isAddingLab = !_isAddingLab;
+                              });
+                            },
+                            icon: Icon(
+                                _isAddingLab
+                                    ? Icons.remove_circle
+                                    : Icons.add_circle,
                                 color: Colors.white),
-                            label: const Text(
-                              "Add New Lab",
-                              style: TextStyle(color: Colors.white),
+                            label: Text(
+                              _isAddingLab ? "Cancel" : "Add New Lab",
+                              style: const TextStyle(color: Colors.white),
                             ),
                             style: ButtonStyle(
                               shape: MaterialStatePropertyAll(
@@ -198,11 +230,143 @@ class _HomePageState extends State<HomePage> {
                                 ),
                               ),
                               backgroundColor: MaterialStateProperty.all(
-                                  Theme.of(context).colorScheme.onPrimary),
+                                  _isAddingLab
+                                      ? Theme.of(context).colorScheme.error
+                                      : Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary),
                             ),
                           ),
                         ],
                       ),
+                      const SizedBox(height: 15),
+                      _isAddingLab
+                          ? Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 250,
+                                      height: 40,
+                                      child: TextField(
+                                        onSubmitted: (value) {
+                                          _labCodeNumber =
+                                              _labCodeNumController.text;
+                                          validateLab(_labCodeNumber!);
+                                        },
+                                        controller: _labCodeNumController,
+                                        decoration: const InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            label: Text("Lab Code...")),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    // ElevatedButton(
+                                    //     onPressed: () {
+                                    //       //fetch and push
+                                    //     },
+                                    //     child: const Text('Add Lab'))
+                                  ],
+                                ),
+                                const SizedBox(height: 20),
+                                _labCode != null
+                                    ? Table(
+                                        columnWidths: const <int,
+                                            TableColumnWidth>{
+                                          0: FixedColumnWidth(250),
+                                          1: FixedColumnWidth(250),
+                                          2: FixedColumnWidth(1000),
+                                        },
+                                        children: [
+                                          const TableRow(children: [
+                                            TableCell(
+                                              child: SizedBox(
+                                                height: 30,
+                                                child: Row(
+                                                  children: [
+                                                    Text(
+                                                      "Lab Code",
+                                                      style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
+                                                    ),
+                                                    SizedBox(width: 5),
+                                                    Icon(Icons
+                                                        .keyboard_hide_outlined),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            TableCell(
+                                              child: SizedBox(
+                                                height: 30,
+                                                child: Row(
+                                                  children: [
+                                                    Text(
+                                                      "Lab Name",
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold),
+                                                    ),
+                                                    SizedBox(width: 5),
+                                                    Icon(Icons
+                                                        .remove_red_eye_outlined)
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            TableCell(
+                                              child: SizedBox(
+                                                height: 30,
+                                                child: Row(
+                                                  children: [
+                                                    Text(
+                                                      "Address",
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold),
+                                                    ),
+                                                    SizedBox(width: 5),
+                                                    Icon(Icons
+                                                        .location_on_outlined)
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ]),
+                                          TableRow(children: [
+                                            TableCell(
+                                              child: Text(
+                                                _labCode![0]['customer_code'],
+                                              ),
+                                            ),
+                                            TableCell(
+                                              child: Text(
+                                                _labCode![0]['address_name'],
+                                              ),
+                                            ),
+                                            TableCell(
+                                                child: Text(_labCode![0]
+                                                        ['addr2'] +
+                                                    " " +
+                                                    _labCode![0]['addr3'] +
+                                                    " " +
+                                                    _labCode![0]
+                                                        ['country_code']))
+                                          ])
+                                        ],
+                                      )
+                                    : Container(
+                                        decoration: const BoxDecoration(
+                                            color: Colors.red),
+                                        child:
+                                            Text(_error != null ? _error! : ""),
+                                      ),
+                              ],
+                            )
+                          : Text("ERROR"),
                     ],
                   )
                 : Container(
